cbc710385886c0c9e5d644b1934cf60641d038c2
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-04 13:19:41 +0000

    fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,6 +5,7 @@ class Like < ApplicationRecord
   after_save :increment_likes_counter
 
   private
+  
   Comment.create(post: first_post, author: second_user)
   def increment_likes_counter
     post.increment!(:likes_counter)
