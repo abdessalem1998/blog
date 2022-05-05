95b3dd8f03722c27533b559ef80a9982ef236197
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-05 21:09:32 +0000

    submit comment logic

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(text: params[:text], author: current_user, post: @post)
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @post.author.id, id: @post.id)
+    else
+      render user_post_path(user_id: @post.author.id, post_id: @post.id), alert: 'An error has occurred while creating the comment.'
+    end
+
+  end
+
+
+end
