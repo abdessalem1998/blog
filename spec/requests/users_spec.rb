8187546f2df857ea99506706c5c6e6fc0973ef85
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-12 13:36:40 +0100

    Adjust the tests to new data

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
     before(:each) do
+      first_user = User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com', password: 'password')
+      first_post = Post.create!(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
       get users_path
     end
 
@@ -13,11 +15,17 @@ RSpec.describe 'Users', type: :request do
     it 'renders a correct template' do
       expect(response).to render_template(:index)
     end
+
+    it 'renders content correctly' do
+      expect(response.body).to include('Tom')
+    end
   end
 
   describe 'GET #show' do
     before(:each) do
-      get user_path(id: 1)
+      first_user = User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com', password: 'password')
+      first_post = Post.create!(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
+      get user_path(id: first_user.id)
     end
 
     it 'handles GET requests' do
@@ -26,6 +34,10 @@ RSpec.describe 'Users', type: :request do
 
     it 'renders a correct template' do
       expect(response).to render_template(:show)
+    end
+
+    it 'renders content correctly' do
+      expect(response.body).to include('Tom')
     end
   end
 end
