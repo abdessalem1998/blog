5a62f1c2fb9692198d73cdfe86466014d7767133
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-12 14:13:17 +0100

    index user tests

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Login page', type: :feature do
   before(:each) do
-    first_user = User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com', password: 'password')
+    first_user = User.create!(name: 'Tom', photo: 'photo.jpg', bio: 'Teacher from Mexico.', email: 'to@example.com', password: 'password', confirmed_at: Time.now)
     visit user_session_path
   end
 
@@ -13,7 +13,7 @@ RSpec.describe 'Login page', type: :feature do
   end
 
   it 'Show error when submit with Empty fields' do
-    fill_in 'email', with: ' '
+    fill_in 'Email', with: ' '
     fill_in 'Password', with: ' '
     click_button 'Log in'
 
@@ -21,11 +21,18 @@ RSpec.describe 'Login page', type: :feature do
   end
 
   it 'Show error when submit with wrong fields values' do
-    fill_in 'email', with: 'wrong'
+    fill_in 'Email', with: 'wrong'
     fill_in 'Password', with: 'wrong'
     click_button 'Log in'
-    expect(page).to have_current_path(new_user_session_path)
+    expect(page).to have_current_path(user_session_path)
     expect(page).to have_content('Invalid Email or password.')
+  end
+
+  it "When I click the submit button after filling in the username and the password with correct data, I am redirected to the root page." do
+    fill_in "Email",	with: "to@example.com"
+    fill_in "Password",	with: "password"
+    click_on 'Log in'
+    expect(current_path).to eq root_path
   end
 
 end
