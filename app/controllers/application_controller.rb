7bda32180599e4fa01c6f2e4f1f7ac3599f156ea
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-13 15:33:12 +0100

    Fix linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include JsonWebToken
-
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,6 @@
 class AuthenticationController < ApplicationController
   protect_from_forgery with: :null_session
   skip_before_action :authenticate_request, only: %i[login register]
-  # POST /auth/login
 
   def register
     @user = User.create! name: params[:name], email: params[:email], password: params[:password], bio: params[:bio],
@@ -9,7 +8,7 @@ class AuthenticationController < ApplicationController
 
     render json: { user: @user }, status: :ok
   end
-  
+
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
