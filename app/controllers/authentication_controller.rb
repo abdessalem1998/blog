cb012ae400e18d0ad310915145bc6849ba58ca9d
ginohmk <michotall95@gmail.com>
2022-05-13 14:31:24 +0100

    Add authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,15 @@
 class AuthenticationController < ApplicationController
   protect_from_forgery with: :null_session
-  skip_before_action :authenticate_request, only: [:login]
+  skip_before_action :authenticate_request, only: %i[login register]
   # POST /auth/login
+
+  def register
+    @user = User.create! name: params[:name], email: params[:email], password: params[:password], bio: params[:bio],
+                         photo: params[:photo], confirmed_at: Time.now
+
+    render json: { user: @user }, status: :ok
+  end
+  
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
